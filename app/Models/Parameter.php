3dbfad1bca2670e9322969047249ee8b2197ae03
Matthi <s0173395@student.thomasmore.be>
2023-04-25 19:29:48 +0200

    FIET-41 aangepast naar Parameter

--- app/Models/Parameter.php
+++ app/Models/Parameter.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Parameters extends Model
+class Parameter extends Model
 {
     use HasFactory;
 
--- database/factories/ParameterFactory.php
+++ database/factories/ParameterFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Parameters>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Parameter>
  */
-class ParametersFactory extends Factory
+class ParameterFactory extends Factory
 {
     /**
      * Define the model's default state.
