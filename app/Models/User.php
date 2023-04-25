fcea59d12b220a47079d211fb006d2e293a65d1b
Matthi <s0173395@student.thomasmore.be>
2023-04-25 15:17:37 +0200

    FIET-24 User Model & Migration aangepast

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,8 +25,12 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'username',
         'email',
         'password',
+        'postal_code',
+        'address',
+        'phone_number',
     ];
 
     /**
