c6458d55c48c23ffcdb2a341e1a101229d2b3b4f
Matthi <s0173395@student.thomasmore.be>
2023-04-25 16:48:51 +0200

    FIET-42 datatype description aangepast

--- database/migrations/2023_04_25_143737_create_texts_table.php
+++ database/migrations/2023_04_25_143737_create_texts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('texts', function (Blueprint $table) {
             $table->id();
-            $table->string('description')->nullable(false);
+            $table->mediumText('description')->nullable(false);
             $table->timestamps();
         });
     }
