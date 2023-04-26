c7757ee648ee9615b92c76158249b19385087f31
Michel Labruyere <michellabruyere@protonmail.com>
2023-04-26 22:45:42 +0200

    FIET-28 seeders, models, factories, en unittesten voor Size, Product en ProductSize aangepast

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -3,23 +3,34 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
+use App\Models\Size;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Runs the database seeder for Products and also seeds the sizes and pivot table (productsizes).
+     * An array sizes is declared and assigned to the sizes table.
+     * The sizes are then randomly selected and assigned to the products table.
+     * The pivot table is also updated, but never any doubles (like 2x t-shirt xs).
+     * This is because of the syncWithoutDetaching method.
      */
     public function run(): void
     {
-        Product::factory()->create();
+        $sizes = ['extra small', 'small', 'medium', 'large', 'extra large'];
+        $randomsize = rand(1, count($sizes));
 
-//        Product::factory(1)->create([
-//            'name' => 'Lovely Overpriced Product',
-//            'price' => 100,
-//        ]);
+        foreach ($sizes as $size)
+        {
+            Size::firstOrCreate(['size' => $size]);
+        }
+
+        $products = Product::factory(100)->create();
+
+        foreach ($products as $product) {
+            $sizes = Size::inRandomOrder()->take($randomsize)->pluck('id');
+            $product->sizes()->syncWithoutDetaching($sizes);
+        }
     }
-
-
 }
