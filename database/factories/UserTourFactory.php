76cc31f79b2f197733811ed4a2be55d99a19462a
Matthi <s0173395@student.thomasmore.be>
2023-04-28 13:38:21 +0200

    FIET-120 factory aangemaakt

--- database/factories/UserTourFactory.php
+++ database/factories/UserTourFactory.php
@@ -2,13 +2,19 @@
 
 namespace Database\Factories;
 
+use App\Models\GroupTour;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\UserTour;
+use App\Models\User;
+use App\Models\Tour;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\UserTour>
  */
 class UserTourFactory extends Factory
 {
+
+    protected $model = UserTour::class;
     /**
      * Define the model's default state.
      *
@@ -17,7 +23,15 @@ class UserTourFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => function() {
+                return User::factory()->create()->id;
+            },
+            'grouptour_id' => function() {
+                return GroupTour::factory()->create()->id;
+            },
+            'tour_id' => function() {
+                return Tour::factory()->create()->id;
+            },
         ];
     }
 }
