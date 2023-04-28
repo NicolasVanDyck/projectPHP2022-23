7e50260ac836d9d5190fd519c213b4eab90d4fae
Nicolas Van Dyck <r0878921@student.thomasmore.be>
2023-04-28 11:07:38 +0200

    FIET-32 userTour model/seeder/factory/controller

--- database/migrations/2023_04_26_172714_create_user_tours_table.php
+++ database/migrations/2023_04_26_172714_create_user_tours_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('user_tours', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->nullable(false)->constrained()->onUpdate('cascade')->onDelete('cascade');
-            $table->foreignId('tour_id')->nullable(false)->constrained()->onUpdate('cascade')->onDelete('cascade');
-            $table->foreignId('groupTour_id')->nullable(false)->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('user_id')->nullable(false)->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('tour_id')->nullable(false)->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('groupTour_id')->nullable(false)->constrained()->cascadeOnUpdate()->cascadeOnDelete();
         });
     }
 
