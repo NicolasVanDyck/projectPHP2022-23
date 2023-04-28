562018aa67a15f1aff57bf58b7b17ae0390e7896
Tom Gijsbers <r0878924@student.thomasmore.be>
2023-04-28 13:56:55 +0200

    FIET-123 Text Factory + text Seeder

--- database/seeders/TextSeeder.php
+++ database/seeders/TextSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Text;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,7 @@ class TextSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Text::factory(5)->create();
+
     }
 }
