8a6277c96e7cd9dbb322be44650b7422f9dd87a7
Matthi <s0173395@student.thomasmore.be>
2023-04-28 13:50:35 +0200

    FIET-121 Tour factory ingevuld

--- database/factories/TourFactory.php
+++ database/factories/TourFactory.php
@@ -3,12 +3,16 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Tour;
+use App\Models\Route;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tour>
  */
 class TourFactory extends Factory
 {
+
+    protected $model  = Tour::class;
     /**
      * Define the model's default state.
      *
@@ -17,7 +21,12 @@ class TourFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'route_id' => function() {
+                return Route::factory()->create()->id;
+            },
+            'startDate' => $this->faker->dateTime('now'),
+            'endDate' => $this->faker->dateTimeBetween('now', '+1 years'),
+
         ];
     }
 }
