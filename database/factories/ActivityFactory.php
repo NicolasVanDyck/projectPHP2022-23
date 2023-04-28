083d29cde7ea30231ca85d735fa128d97a2df2ae
Tom Gijsbers <r0878924@student.thomasmore.be>
2023-04-28 14:15:00 +0200

    protected classes

--- database/factories/ActivityFactory.php
+++ database/factories/ActivityFactory.php
@@ -3,12 +3,14 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use App\Models\Activity;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Activity>
  */
 class ActivityFactory extends Factory
 {
+
+    protected $model  = Activity::class;
     /**
      * Define the model's default state.
      *
