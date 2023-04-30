dc9012cb69ce1d7a4e26bd1d645d9a51dffe9867
Tom Gijsbers <r0878924@student.thomasmore.be>
2023-04-30 14:41:38 +0200

    complete form + werkende dropdown

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,14 @@ import focus from '@alpinejs/focus';
 // Initialization for ES Users
 import {
     Carousel,
+    Dropdown,
+    Ripple,
+    Input,
+    Select,
     initTE,
 } from "tw-elements";
 
-initTE({ Carousel });
+initTE({ Carousel, Dropdown, Ripple, Input, Select });
 
 window.Alpine = Alpine;
 
