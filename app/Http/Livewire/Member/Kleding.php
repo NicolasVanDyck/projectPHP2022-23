4c8fa98d587d73dcc0dbc63b53a5d94902b1bdd4
Michel Labruyere <michellabruyere@protonmail.com>
2023-05-07 09:47:17 +0200

    FIET-183 first commit

---
+++ app/Http/Livewire/Member/Kleding.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Member;
+
+use Livewire\Component;
+
+class Kleding extends Component
+{
+    public function render()
+    {
+        return view('livewire.member.kleding');
+    }
+}
