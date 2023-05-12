0bab5d32d64d6dc0b278d17f64665b5eab7514c2
Matthi <s0173395@student.thomasmore.be>
2023-05-12 11:19:22 +0200

    FIET-195 laatste veranderingen

--- app/Http/Livewire/Filter.php
+++ app/Http/Livewire/Filter.php
@@ -4,13 +4,33 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Group;
+use App\Models\GroupTour;
+use App\Models\Tour;
+use App\Models\Route;
 
 class Filter extends Component
 {
+    public $group = "%";
+
+    public $afstand;
+    public $afstandMin, $afstandMax;
+
+    public function mount()
+    {
+        $this->afstandMin = ceil(Route::min('amount_of_km'));
+        $this->afstandMax = ceil(Route::max('amount_of_km'));
+        $this->afstand = $this->afstandMax;
+    }
+
     public function render()
     {
-        $groups = Group::all();
+        $routes = Route::orderBy('id')->has('tours')->get();
+        $grouptours = GroupTour::orderBy('id')
+            ->where('group_id','like',$this->group)
+            ->get();
+        $groups = Group::orderBy('name')->with('grouptours')->get();
 //        $groups = Group::orderBy('id')->has('grouptours')->get();
-        return view('livewire.filter', compact('groups'));
+        return view('livewire.filter', compact('groups', 'grouptours', 'routes'));
     }
+
 }
--- resources/views/livewire/filter.blade.php
+++ resources/views/livewire/filter.blade.php
@@ -1,17 +1,36 @@
 <h3>Filter op groep:</h3>
-<div class="container flex">
+<div class="container flex justify-between mx-auto">
     <div>
-        <select id="groups">
-            @foreach($groups as $group)
-                <option value="{{ $group->id }}">{{ $group->name }}</option>
+        <label for="group" value="group"/>
+        <select id="group" wire:model="group">
+            <option value="%">Kies hier je groep</option>
+            @foreach($groups as $g)
+                <option value="{{ $g->id }}">{{ $g->name }}</option>
             @endforeach
         </select>
     </div>
     <div>
-        <select id="groups">
-            @foreach($groups as $group)
-                <option value="{{ $group->id }}">{{ $group->name }}</option>
-            @endforeach
-        </select>
+        <label for="afstand">Aantal kilometers:
+            <output id="kilometerfilter" name="kilometerfilter">{{$afstand}}</output>
+        </label>
+        <input type="range" id="afstand" name="afstand" wire:model="afstand" min="{{$afstandMin}}" max="{{$afstandMax}}" value="0" step="10"
+               oninput="kilometerfilter.value = afstand.value">
     </div>
+
+
+</div>
+<div class="container flex grow justify-center">
+    @foreach($grouptours as $grouptour)
+        <div wire:key="grouptour_{{$grouptour->id}}"
+             class='flex items-center justify-center bg-gradient-to-br px-2 pb-4 mx'>
+            <div class='w-full max-w-md  mx-auto bg-white rounded-3xl shadow-xl overflow-hidden'>
+                <div class='max-w-md mx-auto'>
+                    <div class='p-8'>
+                        <p class='font-bold text-gray-700 text-[22px] leading-7 mb-1'>{{$grouptour->id}}</p>
+                        <p class='text-[#7C7C80] font-[15px] mt-6'>{{$grouptour->tour_id}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
 </div>
