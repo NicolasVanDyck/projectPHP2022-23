00f07bbf36e2a1ae174689d977237d71cc640c43
Michel Labruyere <michellabruyere@pm.me>
2023-05-12 14:27:19 +0200

    FIET-183 fixed some db settings that were lost in fiet 191

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Order>
@@ -38,14 +39,14 @@ class OrderFactory extends Factory
             'mobile_number' => $this->faker->phoneNumber(),
         ]);
 
-        // Create products if they don't exist yet.
-        $product = Product::updateOrCreate([
-            'name' => $this->faker->name(),
-            'price' => $this->faker->randomDigit(),
-        ]);
+// Get all the productsizes from product_size table.
+        $product_sizes = DB::table('product_size')->get();
+
+        // Get a random product_size from the product_size table.
+        $product_size = $product_sizes->random();
 
         return [
-            'product_id'=> $product->id,
+            'product_size_id'=> $product_size->id,
             'user_id'=> $user->id,
             'order_date'=> now(),
             'quantity'=> $this->faker->randomDigit(),
--- database/migrations/2023_04_25_032844_create_product_size_pivot_table.php
+++ database/migrations/2023_04_25_032844_create_product_size_pivot_table.php
@@ -14,12 +14,21 @@ class CreateProductSizePivotTable extends Migration
     public function up()
     {
         Schema::create('product_size', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('product_id')->index();
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
-            $table->unsignedBigInteger('size_id')->index();
-            $table->foreign('size_id')->references('id')->on('sizes')->onDelete('cascade');
+//            $table->id();
+//            $table->unsignedBigInteger('product_id')->index();
+//            $table->foreign('product_id')->references('id')->on('products')->onDelete('restrict');
+//            $table->unsignedBigInteger('size_id')->index();
+//            $table->foreign('size_id')->references('id')->on('sizes')->onDelete('restrict');
 //            $table->primary(['product_id', 'size_id']);
+
+            // Hier een destrict on delete toegevoegd, in plaats van cascade. Als de maat wordt verwijderd, kan het zijn
+            // ....dat deze nog in een order table staat. Historisch gezien moeten we hier dus een soft delete doen.
+
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->restrictOnDelete();
+            $table->unsignedBigInteger('size_id');
+            $table->foreign('size_id')->references('id')->on('sizes')->restrictOnDelete();
         });
     }
 
