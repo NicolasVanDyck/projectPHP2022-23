9d0f256954b1a687784f029dda1d8fad2e62a446
Nicolas Van Dyck <r0878921@student.thomasmore.be>
2023-05-13 14:14:18 +0200

    FIET-112 Activiteiten kaart aangemaakt

--- app/Http/Controllers/Member/StravaController.php
+++ app/Http/Controllers/Member/StravaController.php
@@ -11,6 +11,7 @@ use Strava;
 class StravaController extends Controller
 {
 
+
     public function stravaAuth($scope = 'read_all,profile:read_all,activity:read_all')
     {
         return Strava::authenticate($scope);
@@ -29,34 +30,45 @@ class StravaController extends Controller
 
     }
 
-    public function test(){
-        //get the user
-        $user = User::find(auth()->user()->id);
-        $years = ['Jan' => 0, 'Feb' => 0, 'Mar' => 0, 'Apr' => 0, 'May' => 0, 'Jun' => 0, 'Jul' => 0, 'Aug' => 0, 'Sep' => 0, 'Oct' => 0, 'Nov' => 0, 'Dec' => 0];
-        // Check if current token has expired
-        if(strtotime(Carbon::now()) > $user->expires_at)
-        {
-            // Token has expired, generate new tokens using the currently stored user refresh token
-            $refresh = Strava::refreshToken($user->refresh_token);
-            // Update the users tokens
-            User::where('id', auth()->user()->id)->update([
-                'access_token' => $refresh->access_token,
-                'refresh_token' => $refresh->refresh_token
-            ]);
-            foreach ($this->getActivities($user->access_token) as $activity){
-                Carbon::parse($activity->start_date)->format('M');
-                $years[Carbon::parse($activity->start_date)->format('M')] += $activity->distance;
+    public function getUserData(){
+        if (auth()->user()->access_token) {
+
+
+            //get the user
+            $user = User::find(auth()->user()->id);
+            $years = ['Jan' => 0, 'Feb' => 0, 'Mar' => 0, 'Apr' => 0, 'May' => 0, 'Jun' => 0, 'Jul' => 0, 'Aug' => 0, 'Sep' => 0, 'Oct' => 0, 'Nov' => 0, 'Dec' => 0];
+            // Check if current token has expired
+            if (strtotime(Carbon::now()) > $user->expires_at) {
+                // Token has expired, generate new tokens using the currently stored user refresh token
+                $refresh = Strava::refreshToken($user->refresh_token);
+                // Update the users tokens
+                User::where('id', auth()->user()->id)->update([
+                    'access_token' => $refresh->access_token,
+                    'refresh_token' => $refresh->refresh_token
+                ]);
+//                foreach ($this->getActivities($user->access_token) as $activity) {
+//                    Carbon::parse($activity->start_date)->format('M');
+//                    $years[Carbon::parse($activity->start_date)->format('M')] += $activity->distance;
+//                }
+//            } else {
+//                foreach ($this->getActivities($user->access_token) as $activity) {
+//                    Carbon::parse($activity->start_date)->format('M');
+//                    $years[Carbon::parse($activity->start_date)->format('M')] += round($activity->distance);
+//                }
             }
-        }else{
-            foreach ($this->getActivities($user->access_token) as $activity){
-                Carbon::parse($activity->start_date)->format('M');
-                $years[Carbon::parse($activity->start_date)->format('M')] += round($activity->distance);
-            }
+            foreach ($this->getActivities($user->access_token) as $activity) {
+                    Carbon::parse($activity->start_date)->format('M');
+                    $years[Carbon::parse($activity->start_date)->format('M')] += round($activity->distance);
+                }
+            $distance = $this->getAthleteStats($user->access_token, $this->getAthleteID($user->access_token))->all_ride_totals->distance;
+            $amount = $this->getAthleteStats($user->access_token, $this->getAthleteID($user->access_token))->all_ride_totals->count;
+            $elevation = $this->getAthleteStats($user->access_token, $this->getAthleteID($user->access_token))->all_ride_totals->elevation_gain;
+            $activities = $this->getActivities($user->access_token);
+//            dd($activities);
+            return view('member.dashboard')->with(compact(['distance', 'amount', 'elevation', 'years', 'activities']));
+        } else {
+            return view('member.dashboard');
         }
-        $distance = $this->getAthleteStats($user->access_token, $this->getAthleteID($user->access_token))->all_ride_totals->distance;
-        $amount = $this->getAthleteStats($user->access_token, $this->getAthleteID($user->access_token))->all_ride_totals->count;
-        $elevation = $this->getAthleteStats($user->access_token, $this->getAthleteID($user->access_token))->all_ride_totals->elevation_gain;
-        return view('member.dashboard')->with(compact(['distance','amount','elevation','years']));
     }
 
     public function getActivities($access_token)
--- resources/views/member/dashboard.blade.php
+++ resources/views/member/dashboard.blade.php
@@ -10,10 +10,11 @@
             <h2 class="text-gray-600 ml-0.5">Vind hier al uw statistieken</h2>
         </div>
     </div>
-
+@if(auth()->user()->access_token == null)
     <div class="flex">
         <button class="font-title text-4xl rounded-lg bg-red-100"><a href="{{ route('stravaAuthentication') }}">Strava</a></button>
     </div>
+@else
     <section class="grid md:grid-cols-2 xl:grid-cols-4 gap-6">
         <div class="flex items-center p-8 bg-white shadow rounded-lg">
             <div class="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 bg-purple-100 rounded-full  mr-6" >
@@ -101,6 +102,40 @@
             </div>
         </div>
     </section>
+    <section>
+        <div class="flex flex-col md:col-span-2 md:row-span-2 bg-white shadow rounded-lg">
+            <div class="px-6 py-5 font-semibold border-b border-gray-100">Activiteiten</div>
+            <div class="flex flex-wrap justify-center">
+                @foreach($activities as $activity)
+                    <x-wd_components.card>
+                        <x-slot:card_title>{{$activity->name}}</x-slot:card_title>
+                        <x-slot:card_date>{{date('d-m-Y',strtotime($activity->start_date))}}</x-slot:card_date>
+                        <x-slot:card_distance>Afstand: {{round($activity->distance/1000,2)}} KM</x-slot:card_distance>
+                        <x-slot:card_time>Tijd: {{gmdate("H:i:s",$activity->elapsed_time)}}</x-slot:card_time>
+                        <x-slot:card_elevation>Hoogtemeters: {{round($activity->total_elevation_gain,2)}} m</x-slot:card_elevation>
+                    </x-wd_components.card>
+                @endforeach
+
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="px-6 py-5 font-semibold border-b border-gray-100">Upload Zone</div>
+        <div class="flex flex-col row-span-3 bg-white shadow rounded-lg">
+            <div class="px-6 py-5 font-semibold border-b border-gray-100">Upload zone fiets statistiek</div>
+            <div class="flex items-center justify-center h-full px-4 py-24  bg-gray-100 border-2 border-gray-200 border-dashed rounded-md">
+                <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                    <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                        <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Klik voor de upload</span> of drag and drop</p>
+                        <p class="text-xs text-gray-500 dark:text-gray-400">.GPX Only</p>
+                    </div>
+                    <input id="dropzone-file" type="file" accept=".gpx," />
+                </label>
+            </div>
+        </div>
+    </section>
+    @endif
 
     {{--            KALENDER/Nog niet ingevuld--}}
     <section class="grid md:grid-cols-2 xl:grid-cols-2 gap-6">
@@ -111,21 +146,6 @@
         <div class="flex flex-col row-span-3 rounded-lg">
             <div class="px-6 py-5 font-semibold border-b border-gray-100">Kalender</div>
             <livewire:activities/>
-        </div>
-
-        UPLOAD ZONE
-        <div class="flex flex-col row-span-3 bg-white shadow rounded-lg">
-            <div class="px-6 py-5 font-semibold border-b border-gray-100">Upload zone fiets statistiek</div>
-            <div class="flex items-center justify-center h-full px-4 py-24  bg-gray-100 border-2 border-gray-200 border-dashed rounded-md">
-                <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                    <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                        <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                        <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Klik voor de upload</span> of drag and drop</p>
-                        <p class="text-xs text-gray-500 dark:text-gray-400">.GPX Only</p>
-                    </div>
-                    <input id="dropzone-file" type="file" type="file" id="Username_file" name="Username_File" accept=".gpx," />
-                </label>
-            </div>
         </div>
     </section>
 </x-templatelayout>
