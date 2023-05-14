0c9a685a7886db7772796979ec72953ab238dc4b
Tom Gijsbers <r0878924@student.thomasmore.be>
2023-05-14 13:34:36 +0200

    FIET-200 Eerste opmaak

--- app/Http/Livewire/GrouptourMember.php
+++ app/Http/Livewire/GrouptourMember.php
@@ -3,14 +3,22 @@
 namespace App\Http\Livewire;
 
 use App\Models\GroupTour;
+use App\Models\Group;
+
 use Livewire\Component;
 
 class GrouptourMember extends Component
 {
     public function render()
     {
-        $groupTours = GroupTour::select('id', 'group_id', 'tour_id', 'start_date','end_date')->get();
+        $groupTours = GroupTour::select('group_tours.id', 'group_tours.group_id', 'group_tours.tour_id', 'group_tours.start_date', 'group_tours.end_date', 'groups.name', 'routes.id as route_id', 'routes.name as route_name', 'routes.amount_of_km')
+            ->join('groups', 'group_tours.group_id', '=', 'groups.id')
+            ->join('tours', 'group_tours.tour_id', '=', 'tours.id')
+            ->join('routes', 'tours.route_id', '=', 'routes.id')
+            ->get();
+
         return view('livewire.grouptour-member', compact('groupTours'));
     }
+
 
 }
--- resources/views/livewire/grouptour-member.blade.php
+++ resources/views/livewire/grouptour-member.blade.php
@@ -1,28 +1,25 @@
-<div>
-    <div class="flex flex-wrap">
-        @foreach($groupTours as $groupTour)
-            <div class="p-4 md:w-1/2 lg:w-1/3">
-                <div class="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
-                    <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="{{ $groupTour->image }}" alt="tour image">
-                    <div class="p-6">
-                        <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Group ID: {{ $groupTour->group_id }}</h2>
-                        <h1 class="title-font text-lg font-medium text-gray-900 mb-3">{{ $groupTour->tour_id }}</h1>
-                        <p class="leading-relaxed mb-3">Start Date: {{ $groupTour->start_date }}</p>
-                        <p class="leading-relaxed mb-3">Start Date: {{ $groupTour->end_date }}</p>
-                        <div class="flex items-center flex-wrap ">
-                            <a href="#" class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Book Now
-                                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                    <path d="M5 12h14"></path>
-                                    <path d="M12 5l7 7-7 7"></path>
-                                </svg>
-                            </a>
-                            <span class="text-gray-400 inline-flex items-center leading-none text-sm ml-3 py-1">{{ $groupTour->id }}</span>
-                        </div>
+<div class="flex flex-wrap">
+    @foreach($groupTours as $groupTour)
+        <div class="p-4 md:w-1/2 lg:w-1/3">
+            <div class="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
+                <div class="relative h-64">
+                    <img class="absolute object-cover w-full h-full" src="{{ $groupTour->route_image }}" alt="{{ $groupTour->route_name }}">
+                </div>
+                <div class="p-6 flex flex-col justify-between">
+                    <div>
+                        <h1 class="font-semibold underline decoration-indigo-500">{{ $groupTour->name }}</h1>
+                        <h2 class="font-semibold underline decoration-indigo-500">{{ $groupTour->route_name }}</h2>
+                        <p class="leading-relaxed text-lg mb-3 pt-1.5">Start Datum: <span class="font-bold">{{ $groupTour->start_date }}</span></p>
+                        <p class="leading-relaxed text-lg mb-3">Eind Datum: <span class="font-bold">{{ $groupTour->end_date }}</span></p>
+                        <p class="leading-relaxed text-lg mb-3">Aantal km: <span class="font-bold">{{ $groupTour->amount_of_km }}</span></p>
+                    </div>
+                    <div class="mt-4">
+                        <a href="#" class="inline-block w-full font-bold rounded bg-info px-6 text-center pb-2 pt-2.5 text-lg uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]">Meld je aan</a>
                     </div>
                 </div>
             </div>
-        @endforeach
-    </div>
-
-
+        </div>
+    @endforeach
 </div>
+
+
