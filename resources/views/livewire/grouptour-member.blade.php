158766909ccf1552de7641e3e5618fade5f79183
Tom Gijsbers <r0878924@student.thomasmore.be>
2023-05-23 18:06:22 +0200

    Finaal

--- resources/views/livewire/grouptour-member.blade.php
+++ resources/views/livewire/grouptour-member.blade.php
@@ -74,12 +74,22 @@
                                 </svg>
                             </button>
                         </div>
-                        <h1 class="text-xl font-bold mb-4 text-center underline decoration-indigo-500 mb-2" id="modal-tour-name">{{ $groupTour->gpx->name }}</h1>
+                        <h1 class="text-xl font-bold mb-4 text-center underline decoration-indigo-500 mb-2" id="modal-tour-name">
+                            {{$groupTour->gpx->name }}
+                        </h1>
                         <img class="rounded-lg w-full mb-4" src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg" alt=""/>
-                        <p class="leading-relaxed text-lg mb-2" id="modal-tour-id">Groep Naam: {{ $groupTour->group->group }}</p>
-                        <p class="leading-relaxed text-lg mb-2" id="modal-start-date">Start Datum: {{ $groupTour->start_date }}</p>
-                        <p class="leading-relaxed text-lg mb-2" id="modal-end-date">Eind Datum: {{ $groupTour->end_date }}</p>
-                        <p class="leading-relaxed text-lg mb-2" id="modal-amount-of-km">Hoeveel Km: {{ $groupTour->gpx->amount_of_km }}</p>
+                        <p class="leading-relaxed text-lg mb-2" id="modal-tour-id">
+                            Groep Naam: {{ $groupTour->group->group }}
+                        </p>
+                        <p class="leading-relaxed text-lg mb-2" id="modal-start-date">
+                            Start Datum: {{ $groupTour->start_date }}
+                        </p>
+                        <p class="leading-relaxed text-lg mb-2" id="modal-end-date">
+                            Eind Datum: {{ $groupTour->end_date }}
+                        </p>
+                        <p class="leading-relaxed text-lg mb-2" id="modal-amount-of-km">
+                            Hoeveel Km: {{ $groupTour->gpx->amount_of_km }}
+                        </p>
                         <div class="mt-4">
                             <button class="inline-block w-full font-bold rounded bg-info px-6 text-center pb-2 pt-2.5 text-lg uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]" wire:click="joinTour({{ $groupTour->tour_id }})">Ik rij mee!</button>
                         </div>
@@ -110,13 +120,13 @@
     </div>
 
 {{--    Aangemelde Groeps ritten--}}
-    <div class="flex justify-center">
+    <div class="flex justify-center w-screen">
         <div class="w-full max-w-3xl bg-white rounded-lg shadow-lg p-6">
             <h1 class="text-2xl font-bold mb-4 text-gray-900 text-center">Geregistreerde Groeps ritten</h1>
             <div>
                 @foreach($userTours as $userTour)
-                    <div class="bg-gray-100 rounded-lg p-4 mb-4">
-                        <p class="text-gray-900 font-bold text-lg mb-2">{{ $userTour->groupTour->gpx->name }}</p>
+                    <div class="bg-gray-100 rounded-lg p-4 mb-4 ">
+                        <p class="text-gray-900 font-bold text-lg underline decoration-indigo-500 mb-2">{{ $userTour->groupTour->gpx->name }}</p>
                         <p class="text-gray-700 text-sm mb-2">Start Datum: {{ $userTour->groupTour->start_date }}</p>
                         <p class="text-gray-700 text-sm mb-2">Eind Datum: {{ $userTour->groupTour->end_date }}</p>
                         <p class="text-gray-700 text-sm mb-2">Aantal Kilometer: {{ $userTour->groupTour->gpx->amount_of_km }} km</p>
