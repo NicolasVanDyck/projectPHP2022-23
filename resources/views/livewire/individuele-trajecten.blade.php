366799779b72e925ba9cfc2b0475b419b2bb6b05
Tom Gijsbers <r0878924@student.thomasmore.be>
2023-05-26 08:53:33 +0200

    indivuele trajceten mobile

--- resources/views/livewire/individuele-trajecten.blade.php
+++ resources/views/livewire/individuele-trajecten.blade.php
@@ -1,19 +1,18 @@
 <div>
-    <div class="flex">
+    <div class="flex flex-col sm:flex-row">
         <div class="flex flex-col">
-        {{-- Filter op gebruiker --}}
-        <h3 class="flex">Filter op leden</h3>
+            {{-- Filter op gebruiker --}}
+            <h3 class="flex">Filter op leden</h3>
             <div class="flex">
                 <label for="user" value="user"/>
                 <select id="user" wire:model="user">
                     <option value="%">Alle Leden</option>
                     @foreach($users as $user)
-                        <option value="{{ $user->name }}">{{ $user->name }} </option>
+                        <option value="{{ $user->name }}">{{ $user->name }}</option>
                     @endforeach
                 </select>
             </div>
         </div>
-
 
         {{-- Filter op afstand --}}
         <div class="flex flex-col">
@@ -33,43 +32,38 @@
     <div class="mt-4">
         {{ $trajecten->links() }}
     </div>
+
+    <!-- Grid of trajects -->
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
         @foreach($trajecten as $traject)
             <div class="p-4">
-            <div
-                class="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-                <a href="#!">
-                    <img
-                        class="rounded-t-lg w-full"
-                        src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"
-                        alt=""/>
-                </a>
-                <div class="p-6">
-                    <h5
-                        class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-                        {{$traject->name}}
-                    </h5>
-                    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                        Afstand: {{round($traject->amount_of_km/1000,2)}}KM
-                    </p>
-                    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                        Uploaded by: {{$traject->user->name}}
-                    </p>
+                <div class="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+                    <a href="#!">
+                        <img class="rounded-t-lg w-full" src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg" alt=""/>
+                    </a>
+                    <div class="p-6">
+                        <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                            {{$traject->name}}
+                        </h5>
+                        <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                            Afstand: {{round($traject->amount_of_km/1000,2)}}KM
+                        </p>
+                        <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                            Uploaded by: {{$traject->user->name}}
+                        </p>
+                    </div>
+                    <div class="flex justify-between">
+                        <x-button wire:click="download('{{$traject->path}}')">DOWNLOAD</x-button>
+                        @if($traject->user->id == auth()->user()->id)
+                            <x-button class="bg-danger-500 hover:bg-danger-600"
+                                      x-data=""
+                                      @click="confirm('Are you sure you want to delete this item?') ? $wire.delete('{{$traject->path}}') : false"
+                            >DELETE
+                            </x-button>
+                        @endif
+                    </div>
                 </div>
-{{--                <div class="flex">--}}
-                    <x-button wire:click="download('{{$traject->path}}')">DOWNLOAD</x-button>
-                    @if($traject->user->id == auth()->user()->id)
-                        <x-button class="bg-danger-500 hover:bg-danger-600"
-                                  x-data=""
-                                  @click="confirm('Are you sure you want to delete this item?') ? $wire.delete('{{$traject->path}}') : false"
-                        >DELETE
-                        </x-button>
-{{--                </div>--}}
-                @endif
-
             </div>
-        </div>
-    @endforeach
-
+        @endforeach
     </div>
 </div>
