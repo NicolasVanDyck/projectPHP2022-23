4ce2f1aa408902ea10051ecdcdcd4d9c9873aec2
Tom Gijsbers <r0878924@student.thomasmore.be>
2023-05-28 17:01:20 +0200

    2

--- app/Http/Livewire/TrajectBeheer.php
+++ app/Http/Livewire/TrajectBeheer.php
@@ -3,25 +3,170 @@
 namespace App\Http\Livewire;
 
 use App\Models\GPX;
+use App\Models\GroupTour;
 use Livewire\Component;
 use App\Models\Group;
-use Livewire\WithPagination;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Validator;
 class TrajectBeheer extends Component
 {
-    use WithPagination;
 
-    protected $paginationTheme = 'bootstrap';
-    public $groups;
-    public $routes;
+    public $selectedRouteName = '';
+    public $selectedGroup;
+    public $selectedRoute = null;
+    public $selectedRouteKm = null;
+    public $selectedDate = null;
+    public $selectedEndDate = null;
+    public $openModal = false;
+    public $isNestedModalOpen = false;
+
+
+
+    public function getGroupTours()
+    {
+        return GroupTour::with(['group', 'tour'])->get();
+    }
 
     public function render()
     {
-        return view('livewire.traject-beheer');
+        $groups = Group::pluck('group', 'id')->toArray();
+        $routes = GPX::all();
+        $groupTours = $this->getGroupTours();
+
+        return view('livewire.traject-beheer', compact('groups', 'routes', 'groupTours'));
     }
-    public function mount()
+
+
+    public function selectGroup($group)
     {
-        $this->groups = Group::pluck('group')->toArray(); // Fetch group names from the database
-        $this->routes = GPX::all(); // Fetch all routes from the database
+        $this->selectedGroup = $group;
+        $this->selectedRoute = null;
+        $this->selectedRouteKm = null;
     }
+
+    public function selectRoute($routeName, $routeKm)
+    {
+        $this->selectedRoute = $routeName;
+        $this->selectedRouteKm = $routeKm;
+        $this->isNestedModalOpen = false;
+    }
+
+    public function closeNestedModal()
+    {
+        $this->isNestedModalOpen = false;
+    }
+
+    public function openModal()
+    {
+        $this->openModal = true;
+    }
+
+    public function closeModal()
+    {
+        $this->openModal = false;
+    }
+
+//    public function createGroepsrit()
+//    {
+//        // Validate the form fields
+//        $validatedData = $this->validate([
+//            'selectedGroup' => 'required',
+//            'selectedRoute' => 'required',
+//            'selectedDate' => 'required',
+//            'selectedEndDate' => 'required',
+//        ]);
+//
+//        // Retrieve the group and route IDs
+//        $groupId = $validatedData['selectedGroup'];
+//        $routeId = $validatedData['selectedRoute'];
+//
+//        // Fetch the Group model
+//        $group = Group::findOrFail($groupId);
+//
+//        // Extract the time from the selected date
+//        $startTime = Carbon::parse($validatedData['selectedDate'])->format('H:i:s');
+//
+//        // Create a new GroupTour entry
+////        $groepsrit = GroupTour::create([
+////            'group_id' => $group->id,
+////            'tour_id' => $routeId,
+////            'start_date' => $validatedData['selectedDate'],
+////            'start_time' => $startTime,
+////            'end_date' => $validatedData['selectedEndDate'],
+////        ]);
+//
+//        // Dump the values being sent to the database
+//        dd([
+//            'group_id' => $group->id,
+//            'tour_id' => $routeId,
+//            'start_date' => $validatedData['selectedDate'],
+//            'start_time' => $startTime,
+//            'end_date' => $validatedData['selectedEndDate'],
+//        ]);
+//
+//        // Reset the form fields after successful submission
+//        $this->selectedGroup = '';
+//        $this->selectedRoute = null;
+//        $this->selectedRouteKm = null;
+//        $this->selectedDate = null;
+//        $this->selectedEndDate = null;
+//
+//        // Close the modal
+//        $this->openModal = false;
+//    }
+
+    public function createGroepsrit()
+    {
+        // Retrieve the form fields
+        $selectedGroup = $this->selectedGroup;
+        $selectedRoute = $this->selectedRoute;
+        $selectedDate = $this->selectedDate;
+        $selectedEndDate = $this->selectedEndDate;
+
+        // Retrieve the group and route IDs
+        $groupId = $selectedGroup;
+        $routeId = $selectedRoute;
+
+        // Fetch the Group model
+        $group = Group::findOrFail($groupId);
+
+        // Extract the time from the selected date
+        $startTime = Carbon::parse($selectedDate)->format('H:i:s');
+
+//         Create a new GroupTour entry
+//        $groepsrit = GroupTour::create([
+//            'group_id' => $group->id,
+//            'tour_id' => $routeId,
+//            'start_date' => $selectedDate,
+//            'start_time' => $startTime,
+//            'end_date' => $selectedEndDate,
+//        ]);
+
+//         Dump the values being sent to the database
+        dd([
+            'group_id' => $group->id,
+            'tour_id' => $routeId,
+            'start_date' => $selectedDate,
+            'start_time' => $startTime,
+            'end_date' => $selectedEndDate,
+        ]);
+
+        // Reset the form fields after successful submission
+        $this->selectedGroup = '';
+        $this->selectedRoute = null;
+        $this->selectedRouteKm = null;
+        $this->selectedDate = null;
+        $this->selectedEndDate = null;
+
+        // Close the modal
+        $this->openModal = false;
+    }
+
+
+
+
+
+
+
 
 }
--- resources/views/livewire/traject-beheer.blade.php
+++ resources/views/livewire/traject-beheer.blade.php
@@ -1,86 +1,149 @@
 <div>
-
-{{--TODO: Selecter voor een startdatum en einddatum en voor een starttijd.--}}
-
-
-
-
-
-
-    <div x-data="{ isOpen: false, isNestedModalOpen: false, selectedGroup: '', selectedRoute: null, selectedRouteKm: null, selectedDate: null }">
-        <x-button @click="isOpen = true" class="bg-danger">Maak je groepsrit</x-button>
-
-        <div x-show="isOpen" x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="fixed inset-0 z-10 bg-gray-900 bg-opacity-50 flex items-center justify-center px-4">
-            <div class="bg-white rounded-lg p-6">
-                <div class="flex justify-end">
-                    <button class="text-gray-500 hover:text-gray-700" @click="isOpen = false">
-                        <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-                        </svg>
-                    </button>
-                </div>
-                <h1 class="text-xl font-bold mb-4 text-center" id="modal-title">Maak een groepsrit</h1>
-                <p>Hier maak je een groeprit aan. Dit doe je doormiddel van het kiezen van een groep en een route.</p>
-                <select class="form-control" x-model="selectedGroup">
-                    <option value="">-- Select Group --</option>
-                    @foreach($groups as $group)
-                        <option value="{{ $group }}">{{ $group }}</option>
-                    @endforeach
-                </select>
-
-                <button @click="isNestedModalOpen = true" class="btn bg-info mt-4" x-show="selectedGroup" :disabled="!selectedGroup">Kies een route</button>
-                <p class="mt-2" x-show="selectedRoute">Geselecteerde route: <span x-text="selectedRoute"></span></p>
-                <p class="mt-2" x-show="selectedRouteKm">Afstand: <span x-text="selectedRouteKm"></span> km</p>
-
-                <template x-if="selectedRoute">
-                    <div class="mt-4">
-                        <label for="start-datepicker" class="block font-medium text-gray-700">Start datum en tijd:</label>
-                        <input type="datetime-local" id="start-datepicker" x-model="selectedDate" class="form-control mt-1" :disabled="!selectedRoute">
-                    </div>
-                    <div class="mt-4">
-                        <label for="end-datepicker" class="block font-medium text-gray-700">Eind datum en tijd:</label>
-                        <input type="datetime-local" id="end-datepicker" x-model="selectedEndDate" class="form-control mt-1" :disabled="!selectedRoute">
-                    </div>
-                </template>
-
-            </div>
+    <div>
+        <div>
+            <x-button wire:click="openModal" class="bg-danger">Maak je groepsrit</x-button>
         </div>
 
-        <div x-show="isNestedModalOpen && selectedGroup" x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="fixed inset-0 z-10 bg-gray-900 bg-opacity-50 flex items-center justify-center px-4">
-            <div class="bg-white rounded-lg p-6">
-                <div class="flex justify-end">
-                    <button class="text-gray-500 hover:text-gray-700" @click="isNestedModalOpen = false">
-                        <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-                        </svg>
-                    </button>
-                </div>
-                <h1 class="text-xl font-bold mb-4 text-center" id="nested-modal-title">Selecteer hier de route</h1>
-                <p class="leading-relaxed" id="nested-modal-description">Hier vindt je alle routes die voor de wezeldrivers beschikbaar zijn</p>
-
-                <div class="grid grid-cols-3 gap-4 mt-4">
-                    @foreach($routes as $route)
-                        <div class="bg-gray-100 p-4 rounded-lg" x-bind:class="{ 'bg-blue-200': selectedRoute === '{{ $route->name }}' }" x-on:click="selectedRoute = '{{ $route->name }}'; selectedRouteKm = '{{ $route->amount_of_km }}'">
-                            <h2 class="text-lg font-bold">{{ $route->name }}</h2>
-                            <p>{{ $route->amount_of_km }} km</p>
+        <div>
+            <div
+                x-data="{ isNestedModalOpen: @entangle('isNestedModalOpen'), openModal: @entangle('openModal'), selectedRoute: @entangle('selectedRoute'), selectedRouteName: @entangle('selectedRouteName'), selectedRouteKm: @entangle('selectedRouteKm'), selectedDate: @entangle('selectedDate'), selectedEndDate: @entangle('selectedEndDate') }"
+                x-cloak>
+                <div x-show="isNestedModalOpen"
+                     class="fixed inset-0 z-10 bg-gray-900 bg-opacity-50 flex items-center justify-center px-4">
+                    <div class="bg-white rounded-lg p-6">
+                        <div class="flex justify-end">
+                            <button class="text-gray-500 hover:text-gray-700" wire:click="closeNestedModal">
+                                <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                          d="M6 18L18 6M6 6l12 12"></path>
+                                </svg>
+                            </button>
                         </div>
-                    @endforeach
+                        <h1 class="text-xl font-bold mb-4 text-center" id="nested-modal-title">Selecteer hier de
+                            route</h1>
+                        <p class="leading-relaxed" id="nested-modal-description">Hier vind je alle routes die voor de
+                            wezeldrivers beschikbaar zijn</p>
+
+                        <div class="grid grid-cols-3 gap-4 mt-4">
+                            @foreach($routes as $route)
+                                <div class="bg-gray-100 p-4 rounded-lg cursor-pointer hover:bg-info-200"
+                                     x-bind:class="{ 'bg-blue-200': {{ $route->id }} === selectedRoute }"
+                                     x-on:click="$wire.selectRoute({{ $route->id }}, '{{ $route->amount_of_km }}')"
+                                >
+                                    <h2 class="text-lg font-bold">{{ $route->name }}</h2>
+                                    <p>{{ $route->amount_of_km }} km</p>
+                                </div>
+                            @endforeach
+                        </div>
+
+                        <button class="btn btn-primary mt-4" x-on:click="$wire.closeNestedModal"
+                                :disabled="!selectedRoute">Selecteer
+                        </button>
+                    </div>
                 </div>
 
-                <button @click="isNestedModalOpen = false" class="btn btn-primary mt-4" :disabled="!selectedRoute">Selecteer</button>
+                <div x-show="openModal"
+                     class="fixed inset-0 z-10 bg-gray-900 bg-opacity-50 z-[1] flex items-center justify-center px-4">
+                    <div class="bg-white rounded-lg p-6">
+                        <div class="flex justify-end">
+                            <button class="text-gray-500 hover:text-gray-700" wire:click="closeModal">
+                                <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                          d="M6 18L18 6M6 6l12 12"></path>
+                                </svg>
+                            </button>
+                        </div>
+                        <h1 class="text-xl font-bold mb-4 text-center" id="modal-title">Maak een groepsrit</h1>
+                        <p>Hier maak je een groeprit aan. Dit doe je door middel van het kiezen van een groep en een
+                            route.</p>
+                        <select wire:model="selectedGroup" class="form-control">
+                            <option value="">-- Kies de groep --</option>
+                            @foreach($groups as $groupId => $group)
+                                <option value="{{ $groupId }}">{{ $group }}</option>
+                            @endforeach
+                        </select>
+
+                        @error('selectedGroup')
+                        <div class="text-red-500 mt-2">{{ 'Kies hier een groep' }}</div>@enderror
+
+                        <div x-show="selectedGroup" class="mt-4">
+                            <button class="btn bg-info mt-4" x-on:click="isNestedModalOpen = true"
+                                    :disabled="!selectedGroup">Kies een route
+                            </button>
+                            <p class="mt-2" x-show="selectedRoute">Geselecteerde route:
+                                <span>{{ $selectedRouteName }}</span></p>
+                            <p class="mt-2" x-show="selectedRouteKm">Afstand: <span>{{ $selectedRouteKm }}</span> km</p>
+
+                            @if($selectedRoute)
+                                <div class="mt-4">
+                                    <label for="start-datepicker" class="block font-medium text-gray-700">Start datum en
+                                        tijd:</label>
+                                    <input type="datetime-local" id="start-datepicker" wire:model="selectedDate"
+                                           class="form-control mt-1">
+                                    @error('selectedDate')
+                                    <div
+                                        class="text-red-500 mt-2">{{ 'Geef nog een start datum en start tijd' }}</div>@enderror
+                                </div>
+                                <div class="mt-4">
+                                    <label for="end-datepicker" class="block font-medium text-gray-700">Eind datum en
+                                        tijd:</label>
+                                    <input type="datetime-local" id="end-datepicker" wire:model="selectedEndDate"
+                                           class="form-control mt-1">
+                                    @error('selectedEndDate')
+                                    <div
+                                        class="text-red-500 mt-2">{{'Geef nog een eind datum en eind tijd' }}</div>@enderror
+                                </div>
+                            @endif
+                        </div>
+
+                        <button class="bg-cyan-300 p-2" wire:click="createGroepsrit"
+                                :disabled="!selectedGroup || !selectedRoute || !selectedDate || !selectedEndDate">Maak
+                            je groepsrit
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+</div>
 
-
-
-
-
-
-
-
-
-
+<div class="p-6">
+    <h1 class="text-xl font-bold underline text-center">Groepsritten</h1>
+    <div class="overflow-x-auto">
+        <table class="table-auto w-full">
+            <thead>
+            <tr>
+                <th class="px-4 py-2">Groep</th>
+                <th class="px-4 py-2">Route</th>
+                <th class="px-4 py-2">Afstand (km)</th>
+                <th class="px-4 py-2">Start Datum</th>
+                <th class="px-4 py-2">Start Tijd</th>
+                <th class="px-4 py-2">Eind Datum</th>
+                <th class="px-4 py-2">Bewerken/Verwijderen</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($groupTours as $groupTour)
+                <tr class="text-center">
+                    <td class="border-y border-gray-700 py-2">{{ $groupTour->group->group }}</td>
+                    <td class="border-y border-gray-700 px-4 py-2">{{ $groupTour->gpx->name }}</td>
+                    <td class="border-y border-gray-700 px-4 py-2">{{ $groupTour->gpx->amount_of_km }}</td>
+                    <td class="border-y border-gray-700 px-4 py-2">{{ $groupTour->start_date }}</td>
+                    <td class="border-y border-gray-700 px-4 py-2">{{ $groupTour->start_time }}</td>
+                    <td class="border-y border-gray-700 px-4 py-2">{{ $groupTour->end_date }}</td>
+                    <td class="border-y border-gray-700 px-4 py-2">
+                        <button wire:click="editGroupTour({{ $groupTour->id }})" class="text-success-500 ml-2">
+                            Bewerken
+                        </button>
+                        <button  class="text-danger-500 ml-2">
+                            Verwijderen
+                        </button>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 
 
