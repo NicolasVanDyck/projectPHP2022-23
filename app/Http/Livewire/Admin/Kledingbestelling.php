dcd8ab0baaacf32b827a2b8ea0059dcd3055ccc2
Michel Labruyere <michellabruyere@pm.me>
2023-06-02 14:47:03 +0200

    FIET-197 update to order

--- app/Http/Livewire/Admin/Kledingbestelling.php
+++ app/Http/Livewire/Admin/Kledingbestelling.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Order;
+use App\Models\User;
 use Livewire\Component;
 
 class Kledingbestelling extends Component
 {
+    public $orders;
+
+    public function mount()
+    {
+        $this->orders = Order::all();
+    }
+
+    public function users()
+    {
+        return User::with('orders')->get();
+    }
+
+    public function orders()
+    {
+        return Order::with('productsizes')->get();
+    }
+
     public function render()
     {
         return view('livewire.admin.kledingbestelling');
--- resources/views/livewire/admin/kledingbestelling.blade.php
+++ resources/views/livewire/admin/kledingbestelling.blade.php
@@ -1,3 +1,25 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+    {{--Shows all the order from the orders table, with user name, product name, amount, product size, product price --}}
+    <table class="mt-2 mb-10">
+        <thead>
+            <tr>
+                <th>Naam</th>
+                <th>Product</th>
+                <th>Aantal</th>
+                <th>Maat</th>
+                <th>Prijs</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($orders as $order)
+                <tr>
+{{--                    @dd($order)--}}
+                    <td>dummyname</td>
+                    <td>{{ $order->product_size_id }}</td>
+                    <td>{{ $order->quantity }}</td>
+                    <td> {{ $order->productsize}}</td>
+                    <td>dummyprice</td>
+                </tr>
+            @endforeach
+    </table>
 </div>
