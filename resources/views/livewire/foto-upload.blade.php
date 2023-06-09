2b54fffee23edfe266da7fd4de767080d4435252
Matthi <s0173395@student.thomasmore.be>
2023-06-09 11:49:32 +0200

    css aangepast

--- resources/views/livewire/foto-upload.blade.php
+++ resources/views/livewire/foto-upload.blade.php
@@ -1,9 +1,9 @@
 <div class="text-black">
-    <div class="bg-white mb-10 py-6 sm:py-8 lg:py-12 shadow-xl rounded-lg bg-slate-100/20">
+    <div class="bg-white mb-4 py-6 sm:py-8 lg:py-12 shadow-xl rounded-lg bg-slate-100/20">
         <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
-            <div class="flex justify-between">
+            <div class="flex flex-col lg:flex-row justify-between">
 {{--                Filter op afbeeldingstype --}}
-                <div>
+                <div class="mb-2">
                     <label for="type" value="type"/>
                     <select id="type" wire:model="type">
                         @foreach($imagetypes as $it)
@@ -15,7 +15,7 @@
 {{--                Filter rit      --}}
 {{--                Waarde aan tour wordt doorgegeven, maar de query wordt niet in rekening genomen?--}}
                 @if($type == 1)
-                <div>
+                <div class="mb-2">
                     <label for="tour" value="tour"/>
                     <select id="tour" wire:model="tour">
                         <option value="%">Alle ritten</option>
@@ -25,19 +25,19 @@
                     </select>
                 </div>
 {{--                Filter: Weergeven op carousel in homepage of niet?      --}}
-                <div>
-                    <label for="homecarousel" value="homecarousel">Getoond op homepage?</label>
+                <div class="mb-2 flex flex-row lg:items-center">
+                    <label class="mr-2" for="homecarousel" value="homecarousel">Getoond op homepage?</label>
                     <x-checkbox id="homecarousel" type="checkbox"
                                 wire:model="homecarousel"
                                 autocomplete="off" class="block mt-1"/>
                 </div>
                 @endif
-                <div>{{$images->links()}}</div>
+                <div class="mb-2">{{$images->links()}}</div>
             </div>
 {{--            Tonen van alle foto's       --}}
             <div class="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 @foreach($images as $image)
-                    <div class="flex-col mb-3"
+                    <div class="flex-col mb-5"
                     wire:key="image_{{$image->id}}"
                     >
 {{--                        Overzicht van info per foto             --}}
@@ -45,9 +45,6 @@
                             <div>
                                 <h3 class="text-lg font-medium text-gray-900">{{$image->name}}</h3>
                                 <p class="text-sm text-gray-500">{{$image->description}}</p>
-                                @if($image->in_carousel)
-                                    <p class="font-bold">Wordt getoond op de homepagina</p>
-                                @endif
                             </div>
                             <div>
                                 @foreach($tours as $tour)
@@ -55,9 +52,9 @@
                                         <p class="text-sm text-gray-500">Tour: '{{$tour->tour_name}}'</p>
                                     @endif
                                 @endforeach
-                                @if($image->image_type_id != null)
-                                    <p class="text-sm text-gray-500">Type: {{$image->image_type_name}}</p>
-                                @endif
+                                    @if($image->in_carousel)
+                                        <x-heroicon-m-home class="h-6 w-6 text-[#073360]"></x-heroicon-m-home>
+                                    @endif
                             </div>
                         </div>
 {{--                        Foto + beschrijving         --}}
@@ -66,12 +63,12 @@
                                  class="w-full h-full object-cover object-center transition duration-300 transform hover:scale-110">
                         </div>
 {{--                        Id doorgeven aan de modal om te kunnen aanpassen               --}}
-                        <div class="mt-3 flex justify-center">
+                        <div class="mt-3 mb-6 flex justify-center">
                             <x-button
                             wire:click="editImage('{{$image->id}}')"
                             >Aanpassen</x-button>
 {{--                        Image verwijderen                   --}}
-                            <x-button bgcolor="rood"
+                            <x-button type="red"
                                       x-data=""
                                       @click="$dispatch('swal:confirm', {
                                       html: 'Verwijder {{ $image->name }}?',
@@ -93,10 +90,10 @@
         </div>
     </div>
 {{--            Uploadzone          --}}
-    <div class="flex justify-center">
+    <div class="flex flex-col mb-3 lg:flex-row justify-center">
         {{--        Welk afbeeldingstype hoort bij de foto's?       --}}
-        <div class="mr-4">
-            <label for="uploadType" value="uploadType">Afbeeldingstype: </label>
+        <div class="mr-4 mb-2">
+            <label class="text-white" for="uploadType" value="uploadType">Afbeeldingstype: </label>
             <select id="uploadType" wire:model="uploadType">
                 @foreach($imagetypes as $itype)
                     <option value="{{ $itype->id }}">{{ $itype->image_type }}</option>
@@ -106,8 +103,8 @@
         </div>
 {{--        Aan welke tour wil je images linken?        --}}
         @if($uploadType == 1)
-        <div class="mr-4">
-            <label for="uploadTour" value="uploadTour">Voor welke tour wil je een afbeelding uploaden? </label>
+        <div class="mr-4 mb-2">
+            <label class="text-white" for="uploadTour" value="uploadTour">Voor welke tour wil je een afbeelding uploaden? </label>
             <select id="uploadTour" wire:model="uploadTour">
                 @foreach($tours as $to)
                     <option value="{{ $to->id }}">{{ $to->tour_name }}</option>
@@ -118,7 +115,7 @@
 {{--        Opslaan en errors       --}}
                     <form wire:submit.prevent="saveImage">
 {{--                        Multiple om aan te duiden dat je meerdere afbeeldingen tegelijkertijd kan uploaden--}}
-                <input type="file" wire:model="photos" multiple>
+                <input class="text-white mb-2" type="file" wire:model="photos" multiple>
                 @if($errors->any())
                     <div class="mb-4">
                         <ul>
@@ -128,7 +125,7 @@
                         </ul>
                     </div>
                 @endif
-                <x-button type="submit">Save Images</x-button>
+                <x-button class="mb-2" type="submit">Save Images</x-button>
             </form>
     </div>
     @include('components.wd_components.modalfotobeheer')
