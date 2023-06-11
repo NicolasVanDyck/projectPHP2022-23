3975e754480b6327b46791c6673c479cd9338489
Matthias Van Rooy <94641591+matthiasvanrooy@users.noreply.github.com>
2023-06-11 10:32:39 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,6 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
-
 class User extends Authenticatable
 {
     use HasApiTokens;
